d0a507872635654cfb30264ef9f3dde3e65aa64f
oumaima <oumaimabenhamou781@gmail.com>
2025-05-05 10:06:31 +0100

    commit

--- logsign/src/main/java/com/example/logsign/controllers/StatistiquesController.java
+++ logsign/src/main/java/com/example/logsign/controllers/StatistiquesController.java
@@ -1,5 +1,16 @@
 package com.example.logsign.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/auth")  // Ajout de cette ligne pour correspondre au chemin /auth/statistiques
 public class StatistiquesController {
-    
+
+    @GetMapping("/statistiques")
+    public String afficherStatistiques() {
+        return "statistiques";  // Correction du nom du template (sans 's')
+    }
 }
